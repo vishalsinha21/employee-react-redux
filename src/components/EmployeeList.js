bd7105fb2570f9b77f1f276823354749c439bd3a
Vishal Sinha <vishal.sinha21@gmail.com>
2017-06-27 11:54:27 +0200

    remove subheader component

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,26 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Employee from './Employee'
+import { Link } from 'react-router';
+
 
 const EmployeeList = ({ employees }) => (
-  <table className="table table-striped">
-    <thead>
-      <tr>
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>Age</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-    {employees.map(employee =>
-        <Employee
-          key={employee.id}
-          employee={employee}
-        />
-    )}
-    </tbody>
-  </table>
+
+    <div>
+      <Link to="/new" className="btn btn-primary">New</Link>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Age</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+            {employees.map(employee => <Employee key={employee.id} employee={employee} />)}
+        </tbody>
+      </table>
+    </div>
 )
 
 export default EmployeeList
--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import Header from './Header'
 import EmployeeListContainer from '../containers/EmployeeListContainer'
-import SubHeader from '../containers/SubHeader'
 
 const List = () => (
   <div className="container">
     <div className="row">
       <div className="col-md-8 col-md-offset-2">
         <Header />
-        <SubHeader />
         <EmployeeListContainer />
       </div>
     </div>
