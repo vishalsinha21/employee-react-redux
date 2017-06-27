1db8b8114be88ef67cfd3f7859691579b7ffd336
Vishal Sinha <vishal.sinha21@gmail.com>
2017-06-27 09:04:24 +0200

    refactor EmployeeEditForm to extend React component

--- src/components/EmployeeEditForm.js
+++ src/components/EmployeeEditForm.js
@@ -2,34 +2,48 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, browserHistory } from 'react-router';
 
-const EmployeeEditForm = ({ employee, onDelete, onEdit }) => (
-  <form className="edit-employee-form" onSubmit={(e) => {
-    console.log(e);
-    e.preventDefault();
-    onDelete(employee.id);
-    browserHistory.push('/')
-  }}>
-    <legend>Edit Employee</legend>
-    <div className="form-group">
-      <label>First Name</label>
-      <input value={employee.firstName} className="form-control" id="firstName" name="firstName" component="input" type="text" placeholder="First Name" />
-    </div>
-    <div className="form-group">
-      <label>Last Name</label>
-      <input value={employee.lastName} className="form-control" id="lastName" name="lastName" component="input" type="text" placeholder="Last Name" />
-    </div>
-    <div className="form-group">
-      <label>Phone</label>
-      <input value={employee.phone} className="form-control" id="phone" name="phone" component="input" type="text" placeholder="Phone" />
-    </div>
-    <hr/>
-    
-      
-    <button type="submit" className="btn">Edit</button>
-    <button type="submit" className="btn">Delete</button>
 
-  </form>
-)
+class EmployeeEditForm extends React.Component {
+
+  constructor() {
+    super();
+    this.submitForm = (employeeId) => ev => {
+      ev.preventDefault();
+      this.props.onDelete(employeeId);
+      browserHistory.push('/')
+    }
+  }
+
+
+  render() {
+    const employee = this.props.employee;
+
+    return (
+        <form className="edit-employee-form" onSubmit={this.submitForm(employee.id)}>
+          <legend>Edit Employee</legend>
+          <div className="form-group">
+            <label>First Name</label>
+            <input value={employee.firstName} className="form-control" id="firstName" name="firstName" component="input" type="text" placeholder="First Name" />
+          </div>
+          <div className="form-group">
+            <label>Last Name</label>
+            <input value={employee.lastName} className="form-control" id="lastName" name="lastName" component="input" type="text" placeholder="Last Name" />
+          </div>
+          <div className="form-group">
+            <label>Phone</label>
+            <input value={employee.phone} className="form-control" id="phone" name="phone" component="input" type="text" placeholder="Phone" />
+          </div>
+          <hr/>
+
+
+          <button type="submit" className="btn">Edit</button>
+          <button type="submit" className="btn">Delete</button>
+
+        </form>
+    );
+  }
+}
+
 
 export default EmployeeEditForm
 
